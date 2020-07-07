81c00b7ca0cc672919b74352a4f93a5633883899
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-07-07 09:13:22 +0800

    Added validation for correct email address

--- src/components/admin_page/AdminRegistration.jsx
+++ src/components/admin_page/AdminRegistration.jsx
@@ -81,10 +81,15 @@ const validate = (values) => {
       errors[field] = "Required";
     }
   });
+
   if (
     values.email &&
     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
   ) {
+    errors.email = "Invalid email address";
+  }
+
+  if (values.email && !values.email.includes("@ciit.edu.ph")) {
     errors.email = "Invalid email address";
   }
   return errors;

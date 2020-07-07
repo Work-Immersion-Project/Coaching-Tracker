c2bf904cc942b43061b585e2b4ae31780183d154
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-07-07 19:30:56 +0800

    Fixed Access Restriction Routes

---
+++ src/components/AccessRestriction.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Route, useRouteMatch } from "react-router-dom";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    height: "100vh",
+    width: "100%",
+    backgroundColor: "#4B4E6D",
+  },
+  containerpaper: {
+    backgroundColor: "#212642",
+    padding: "20px",
+  },
+  line1: {
+    color: "#f05545",
+    paddingBottom: "15px",
+  },
+  line2: {
+    color: "white",
+  },
+}));
+
+const AccessRestriction = (props) => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.content}
+    >
+      <Paper elevation={3} className={classes.containerpaper}>
+        <Typography variant="h3" align="center" className={classes.line1}>
+          Error 403: Unauthorized Access
+        </Typography>
+        <Typography variant="h4" align="center" className={classes.line2}>
+          You are currently logged in as a {props.user.type}
+        </Typography>
+        <Typography variant="h4" align="center" className={classes.line2}>
+          Please sign in with the appropriate account
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default AccessRestriction;
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { Typography } from "@material-ui/core";
+import AccessRestriction from "./AccessRestriction";
+
 const PrivateRoute = ({ component: RouteComponent, ...props }) => {
   return (
     <Route
@@ -17,6 +20,10 @@ const PrivateRoute = ({ component: RouteComponent, ...props }) => {
             />
           );
         } else {
+          const { user } = props;
+          if (!routeProps.location.pathname.includes(user.type)) {
+            return <AccessRestriction user={user} />;
+          }
           return <RouteComponent {...routeProps} />;
         }
       }}
@@ -27,6 +34,7 @@ const PrivateRoute = ({ component: RouteComponent, ...props }) => {
 const mapStateToProps = (state) => {
   return {
     isSignedIn: state.auth.data?.isSignedIn,
+    user: state.auth.data?.user,
   };
 };
 

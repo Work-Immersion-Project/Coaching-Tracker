048439dac70018f411b61c5165cbba914e955dd8
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-07-07 19:30:37 +0800

    Added a working Calendar

--- src/components/teacher_page/TeacherSchedules.jsx
+++ src/components/teacher_page/TeacherSchedules.jsx
@@ -13,11 +13,16 @@ import {
   Paper,
   CircularProgress,
 } from "@material-ui/core";
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  ViewState,
+  EditingState,
+  IntegratedEditing,
+} from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
   DayView,
   WeekView,
+  ViewSwitcher,
   MonthView,
   TodayButton,
   Toolbar,
@@ -33,14 +38,50 @@ const useStyles = makeStyles((theme) => ({
   content: {
     height: "100%",
     width: "100%",
-    padding: "0 1em",
+    padding: "1em",
     backgroundColor: "#4B4E6D",
   },
   scheduler: {
-    height: "50%",
-    width: "100%",
+    height: "100%",
   },
 }));
+
+const handleCellOnClick = (cell) => {
+  console.log(cell);
+};
+
+const WeekTableCell = (props) => {
+  return (
+    <WeekView.TimeTableCell
+      {...props}
+      onClick={() => {
+        handleCellOnClick(props);
+      }}
+    />
+  );
+};
+
+const DayTableCell = (props) => {
+  return (
+    <DayView.TimeTableCell
+      {...props}
+      onClick={() => {
+        handleCellOnClick(props);
+      }}
+    />
+  );
+};
+
+const MonthTableCell = (props) => {
+  return (
+    <MonthView.TimeTableCell
+      {...props}
+      onClick={() => {
+        handleCellOnClick(props);
+      }}
+    />
+  );
+};
 
 const TeacherSchedules = (props) => {
   const classes = useStyles();
@@ -55,15 +96,30 @@ const TeacherSchedules = (props) => {
   const renderContent = () => {
     if (props.calendarEvents) {
       return (
-        <Paper>
-          <Scheduler data={props.calendarEvents}>
-            <ViewState defaultCurrentDate={currentDate} />
-            <MonthView />
+        <Paper className={classes.scheduler}>
+          <Scheduler height="auto" data={props.calendarEvents}>
             <Toolbar />
+            <ViewState
+              defaultCurrentDate={currentDate}
+              defaultCurrentViewName="Week"
+            />
+            <ViewSwitcher />
+
+            <MonthView timeTableCellComponent={MonthTableCell} />
+            <DayView
+              startDayHour={1}
+              endDayHour={24}
+              timeTableCellComponent={DayTableCell}
+            />
+            <WeekView
+              startDayHour={1}
+              endDayHour={24}
+              timeTableCellComponent={WeekTableCell}
+            />
             <DateNavigator />
             <TodayButton />
             <Appointments />
-            <AppointmentTooltip />
+            <AppointmentTooltip showOpenButton showDeleteButton />
           </Scheduler>
         </Paper>
       );

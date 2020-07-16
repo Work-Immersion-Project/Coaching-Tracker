7231ee90c730f375944e770a1e62742aed0e845a
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2020-07-16 18:36:25 +0800

    Fixed duplicate declaration

--- src/actions/coachingActions.jsx
+++ src/actions/coachingActions.jsx
@@ -10,8 +10,7 @@ import {
   ADD_COACHING_SCHEDULE_SUCCESS,
   REMOVE_COACHING_SCHEDULE_REQUEST,
   REMOVE_COACHING_SCHEDULE_SUCCESS,
-  UPDATE_COACHING_SCHEDULE_STATUS_REQUEST,
-  UPDATE_COACHING_SCHEDULE_STATUS_SUCCESS, REQUEST_COACHING_SCHEDULE_REQUEST,
+  REQUEST_COACHING_SCHEDULE_REQUEST,
   REQUEST_COACHING_SCHEDULE_SUCCESS,
   UPDATE_COACHING_SCHEDULE_STATUS_REQUEST,
   UPDATE_COACHING_SCHEDULE_STATUS_SUCCESS,
@@ -27,7 +26,7 @@ import {
 import { showNotification, showModal, hideModal } from "../actions";
 import { normalizeDate } from "../utils";
 import firebase from "firebase";
-import {db} from "../firebase"
+import { db } from "../firebase";
 import { result } from "lodash";
 import { setError } from "./errorActions";
 

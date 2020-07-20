3f33050e4571f0b1bf126961c0af476cb1ef7cc0
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2020-07-20 13:59:32 +0800

    Updates

---
+++ precache-manifest.67caea7395f45cc095893dc5746d4b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ace9d825311ee5e01a6473b13a8bb5",
+    "url": "/Coaching-Tracker/index.html"
+  },
+  {
+    "revision": "972c923d68f4eb26a73e",
+    "url": "/Coaching-Tracker/static/css/main.ac428dc0.chunk.css"
+  },
+  {
+    "revision": "486354ac88d792354f11",
+    "url": "/Coaching-Tracker/static/js/2.55213fe9.chunk.js"
+  },
+  {
+    "revision": "7da8be4199c985d00c41003acec262d6",
+    "url": "/Coaching-Tracker/static/js/2.55213fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972c923d68f4eb26a73e",
+    "url": "/Coaching-Tracker/static/js/main.6292f39c.chunk.js"
+  },
+  {
+    "revision": "3226a6550984958abeb0",
+    "url": "/Coaching-Tracker/static/js/runtime-main.bb1d85c1.js"
+  }
+]);

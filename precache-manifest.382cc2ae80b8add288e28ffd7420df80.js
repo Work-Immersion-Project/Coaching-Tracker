d7f37b58e9c4adcf2c3ad38ebfc4693a88610b26
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-08-06 05:46:08 +0800

    Updates

---
+++ precache-manifest.382cc2ae80b8add288e28ffd7420df80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fb19699c5e3fc0b1803f36d3a190a0",
+    "url": "/Coaching-Tracker/index.html"
+  },
+  {
+    "revision": "ba4254d69f93a3efd98c",
+    "url": "/Coaching-Tracker/static/css/main.ac428dc0.chunk.css"
+  },
+  {
+    "revision": "5d1974baaf2b26a945ad",
+    "url": "/Coaching-Tracker/static/js/2.ee8bdbd8.chunk.js"
+  },
+  {
+    "revision": "7da8be4199c985d00c41003acec262d6",
+    "url": "/Coaching-Tracker/static/js/2.ee8bdbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4254d69f93a3efd98c",
+    "url": "/Coaching-Tracker/static/js/main.ce1c603a.chunk.js"
+  },
+  {
+    "revision": "3226a6550984958abeb0",
+    "url": "/Coaching-Tracker/static/js/runtime-main.bb1d85c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a829f33273de33a87e9b38d9f544a155.js"
+  "/Coaching-Tracker/precache-manifest.382cc2ae80b8add288e28ffd7420df80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Coaching-Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

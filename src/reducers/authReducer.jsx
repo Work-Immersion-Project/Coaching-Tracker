9050297b107fa9e961e0bf72b6a3116d61dae72e
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-08-08 13:42:47 +0800

    Removed unused types from the auth action types

--- src/reducers/authReducer.jsx
+++ src/reducers/authReducer.jsx
@@ -1,10 +1,8 @@
 import {
   AUTH_SIGN_IN_REQUEST,
   AUTH_SIGN_IN_SUCCESS,
-  AUTH_SIGN_IN_ERROR,
   AUTH_SIGN_OUT_REQUEST,
   AUTH_SIGN_OUT_SUCCESS,
-  AUTH_SIGN_OUT_ERROR,
 } from "../types";
 
 const INITIAL_STATE = {
@@ -15,17 +13,11 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case AUTH_SIGN_IN_REQUEST:
+    case AUTH_SIGN_OUT_REQUEST:
+    case AUTH_SIGN_OUT_SUCCESS:
       return { ...state };
     case AUTH_SIGN_IN_SUCCESS:
       return { ...state, data: action.payload, error: null };
-    case AUTH_SIGN_IN_ERROR:
-      return { ...state, data: null, error: action.error };
-    case AUTH_SIGN_OUT_REQUEST:
-      return { ...state };
-    case AUTH_SIGN_OUT_SUCCESS:
-      return { ...state, data: action.payload, error: null };
-    case AUTH_SIGN_OUT_ERROR:
-      return { ...state, data: null, error: action.error };
     default:
       return state;
   }

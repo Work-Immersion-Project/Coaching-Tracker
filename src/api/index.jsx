46ff461af6ad39db182cb21dcc4feb90dd468cfd
Paolo Matthew Tolentino <tolentinopaolomatthew23@gmail.com>
2020-08-16 16:01:15 +0800

    Fixed Unsecured Connection URL

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { API_BASE_URL } from "../consts/api";
 
 export default Axios.create({
-  baseURL: `http://${API_BASE_URL}`,
+  baseURL: `https://${API_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
   },

663f3d8bfbcc7e1579ddd254ed5aa23e471ee6d1
Paolo Tolentino <tolentinopaolomatthew23@gmail.com>
2020-08-17 15:20:30 +0800

    Fixed unsecured connection

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,9 +1,18 @@
 import Axios from "axios";
 import { config } from "../consts/config";
 
-export default Axios.create({
+const prod = Axios.create({
+  baseURL: `https://${config.API_BASE_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const dev = Axios.create({
   baseURL: `http://${config.API_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+export default process.env.NODE_ENV === "development" ? dev : prod;
--- src/consts/api.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = "coaching-tracker-api.herokuapp.com";
-// export const API_BASE_URL = "localhost:8000";
-export const WEB_SOCKET_BASE_URL = "wss://" + API_BASE_URL;
